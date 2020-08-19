14d98dc150db3dea6f6ac030612a67ee445e4974
YayoiAyame <scarletiris313@gmail.com>
2020-08-19 12:14:51 +0900

    feat: Add type of return value for eventmit. (#1)
    * Add type of return value for eventmit.
    
    * Fix type name Mitter to Eventmitter, and README.md

--- src/eventmit.ts
+++ src/eventmit.ts
@@ -1,28 +1,36 @@
 export type EventmitHandler<T> = (value: T) => any;
-export const eventmit = <T>() => {
+
+export type Eventmitter<T> = {
+    /**
+     * Register an event handler
+     */
+    on: (handler: EventmitHandler<T>) => void;
+    /**
+     * Remove an event handler
+     */
+    off: (handler: EventmitHandler<T>) => void;
+    /**
+     * Remove all event handlers
+     */
+    offAll: () => void;
+    /**
+     * Invoke all handlers
+     */
+    emit: (value: T) => void;
+};
+
+export const eventmit: <T>() => Eventmitter<T> = <T>() => {
     const set = new Set<EventmitHandler<T>>();
     return {
-        /**
-         * Register an event handler
-         */
         on(handler: EventmitHandler<T>) {
             set.add(handler);
         },
-        /**
-         * Remove an event handler
-         */
         off(handler: EventmitHandler<T>) {
             set.delete(handler);
         },
-        /**
-         * Remove all event handlers
-         */
         offAll() {
             set.clear();
         },
-        /**
-         * Invoke all handlers
-         */
         emit(value: T) {
             set.forEach((handler) => handler(value));
         },
